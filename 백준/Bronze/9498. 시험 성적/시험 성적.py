572ff42c7fdcee4848e935df6aeac954a258521d
JiwonDu <djw0615@naver.com>
2024-12-23 19:22:39 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+score = int(input())
+
+grade = (lambda s: 'A' if 90 <= s <= 100 else
+                  'B' if 80 <= s <= 89 else
+                  'C' if 70 <= s <= 79 else
+                  'D' if 60 <= s <= 69 else
+                  'F')(score)
+
+print(grade)
+
+    
